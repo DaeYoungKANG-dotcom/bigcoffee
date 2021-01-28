2e10ba7ab371591bb3b6da87f8e3c7c83898b86f
DaeYoungKANG-dotcom <78129190+DaeYoungKANG-dotcom@users.noreply.github.com>
2021-01-28 15:34:40 +0900

    Add files via upload

---
+++ bigcoffee/JS/main.js
@@ -0,0 +1,12 @@
+/* main js */
+
+$(function(){
+//메인메뉴 열기
+    $('.btn_close').on('click',function(){
+     $('#gnb').show();
+    })
+    //메인메뉴 닫기
+    $('.btn_close').on('click',function(){
+        $('#gnb').hide();
+    });
+});
