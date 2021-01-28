82b450a6da329f071d9575dc79aec6855d5e5299
DaeYoungKANG-dotcom <78129190+DaeYoungKANG-dotcom@users.noreply.github.com>
2021-01-28 16:29:41 +0900

    Add files via upload
    슬라이더 업데이트

--- bigcoffee/JS/main.js
+++ bigcoffee/JS/main.js
@@ -9,4 +9,18 @@ $(function(){
     $('.btn_close').on('click',function(){
         $('#gnb').hide();
     });
+    //이미지 슬라이더 : fade 효과로 할것 //
+    
+    
+    let num = 1;
+    let slider = setInterval(function(){
+        if(num > 2) {num = 0; }
+    //전체이미지 fadeout
+        $('.slider > img').removeClass('show');
+        //다음 이미지 표시
+        $('.slider > img').eq(num).addClass('show');
+
+        console.log(num)
+        num++;
+    },3000);
 });
